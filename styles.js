f7327f147114e23ff9856386983e211a1f59c8a3
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2018-08-14 23:51:07 -0500

    convert to be es2017 compatible

--- styles.js
+++ styles.js
@@ -1,21 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
-const radius = 10;
+var _extends =
+	Object.assign ||
+	function(target) {
+		for (var i = 1; i < arguments.length; i++) {
+			var source = arguments[i]
+			for (var key in source) {
+				if (Object.prototype.hasOwnProperty.call(source, key)) {
+					target[key] = source[key]
+				}
+			}
+		}
+		return target
+	}
+
+const radius = 10
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
+	container: _extends({}, StyleSheet.absoluteFillObject, {
+		backgroundColor: 'transparent',
+		overflow: 'hidden',
+	}),
 
-    backgroundColor: 'transparent',
-    overflow: 'hidden',
-  },
+	ripple: {
+		width: radius * 2,
+		height: radius * 2,
+		borderRadius: radius,
+		overflow: 'hidden',
+		position: 'absolute',
+	},
+})
 
-  ripple: {
-    width: radius * 2,
-    height: radius * 2,
-    borderRadius: radius,
-    overflow: 'hidden',
-    position: 'absolute',
-  },
-});
-
-export { styles, radius };
+export { styles, radius }
